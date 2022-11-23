7e6fbe5bb966242f2c6d7457c497d06229ec1d70
Dyego Lima <dyegodll18@gmail.com>
2022-11-23 16:17:16 -0300

    Backend do Desafio Catalogo de Clientes criado

---
+++ backend/src/main/java/com/devsuperior/client_catalog/ClientCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.client_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientCatalogApplication.class, args);
+	}
+
+}
