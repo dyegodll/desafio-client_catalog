56503c05b36da5ca4732c90671950f31bb93b4ee
Dyego Lima <dyegodll18@gmail.com>
2022-12-04 12:23:39 -0300

    UPADATE Client - PUT OK!

--- backend/src/main/java/com/devsuperior/client_catalog/resourses/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/client_catalog/resourses/exceptions/ResourceExceptionHandler.java
@@ -10,14 +10,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.devsuperior.client_catalog.services.exceptions.EntityNotFoundException;
+import com.devsuperior.client_catalog.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request){
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError();
 		err.setTimestamp(Instant.now());
